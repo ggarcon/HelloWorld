f822b2a9a34925bea7b608013e8cbaa0259b5f72
Gaetan Garcon <gaetangarcon@Gaetans-MacBook-Air.local>
2017-09-18 19:44:49 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Today is Septemebr 18, 2017. I am live!");
+        System.out.println("My name is Gaetan Garcon");
+                }
+}
